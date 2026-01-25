0d3f976e7476d24a94c31dcf1cfebddb9f81dd25
FragozoLeonardo <leo.fragozo.dev@gmail.com>
2026-01-25 10:43:05 -0300

    test: implement book model specs with idiomatic factories and traits

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -3,9 +3,7 @@
 require "rails_helper"
 
 RSpec.describe Book, type: :model do
-  subject { book }
-
-  let(:book) { described_class.new(title: "High Performance Postgres", isbn: "978-1234567890", status: :tsundoku) }
+  subject(:book) { build(:book) }
 
   describe "database schema" do
     it { is_expected.to have_db_column(:title).of_type(:text).with_options(null: false) }
@@ -20,13 +18,27 @@ RSpec.describe Book, type: :model do
     it { is_expected.to validate_presence_of(:title) }
     it { is_expected.to validate_presence_of(:isbn) }
     it { is_expected.to validate_uniqueness_of(:isbn).case_insensitive }
+
+    it "has a valid factory" do
+      expect(book).to be_valid
+    end
+
+    context "with invalid traits" do
+      it "is invalid when using the invalid trait" do
+        expect(build(:book, :invalid)).not_to be_valid
+      end
+    end
   end
 
   describe "enums" do
     it { is_expected.to define_enum_for(:status).with_values(tsundoku: 0, reading: 1, finished: 2) }
 
-    it "raises ArgumentError when setting an invalid status" do
-      expect { book.status = :invalid_option }.to raise_error(ArgumentError)
+    it "allows switching status to reading via trait" do
+      expect(build(:book, :reading)).to be_reading
+    end
+
+    it "allows switching status to finished via trait" do
+      expect(build(:book, :finished)).to be_finished
     end
   end
 end
